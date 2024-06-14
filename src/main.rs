160f65656a2d22b5e6fd80e9bfcea7d9dfe71852
leibuyun <leibuyun@wps.cn>
2024-06-14 18:13:06 +0800

    feaT: min move to seat

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 struct Solution;
 
-fn merge_str(s1: &str, s2: &str) -> String {
-    let mut s = String::from(s1);
-    s.push_str(s2);
-    s
-}
-
-fn gcd(mut m: usize, mut n: usize) -> usize {
-    while n != 0 {
-        let temp = m;
-        m = n;
-        n = temp % n;
-    }
-    m
-}
-
 impl Solution {
-    pub fn gcd_of_strings(str1: String, str2: String) -> String {
-        if merge_str(&str1, &str2) == merge_str(&str2, &str1) {
-            str1[0..gcd(str1.len(), str2.len())].to_string()
-        } else {
-            String::new()
-        }
+    pub fn min_moves_to_seat(seats: Vec<i32>, students: Vec<i32>) -> i32 {
+        let mut seats = seats;
+        seats.sort();
+        let mut students = students;
+        students.sort();
+        seats
+            .iter()
+            .zip(students.iter())
+            .map(|(x, y)| (*x - *y).abs())
+            .sum()
     }
 }
-fn main() {}
+fn main() {
+    println!(
+        "{}",
+        Solution::min_moves_to_seat(vec![12, 14, 19, 19, 12], vec![19, 2, 17, 20, 7])
+    )
+}
